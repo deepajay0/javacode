fd3e0f829582804d454f1385403618838d5512e0
deepajay0 <119274654+deepajay0@users.noreply.github.com>
2025-07-05 16:06:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("New code");
+        System.out.println("New Code V2");
     }
 }
